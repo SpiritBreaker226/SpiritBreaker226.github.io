682b21b167391af401fd7094ab49995e378f56da
Jason Stathopulos <jstathopulos@gmail.com>
2016-07-05 17:55:07 -0400

    Add Stylus As Style Preprocessor For Project

--- react/webpack.config.babel.js
+++ react/webpack.config.babel.js
@@ -4,4 +4,12 @@ module.exports = {
     path: 'dist',
     filename: 'bundle.js',
   },
+  module: {
+    loaders: [
+      {
+        test: /\.styl$/,
+        loader: 'style!css!stylus',
+      },
+    ],
+  },
 }
